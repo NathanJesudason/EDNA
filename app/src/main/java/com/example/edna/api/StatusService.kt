2b04075e713a44029c00ae18f5ec6e8935e3e357
Nathan Jesudason <nathan.jesudason@gmail.com>
2023-03-23 02:02:01 -0700

    add interceptor to close requests

--- app/src/main/java/com/example/edna/api/StatusService.kt
+++ app/src/main/java/com/example/edna/api/StatusService.kt
@@ -1,6 +1,7 @@
 package com.example.edna.api
 
 import com.example.edna.data.Status
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Response
 import retrofit2.Retrofit
@@ -11,22 +12,27 @@ import java.net.InetSocketAddress
 import java.net.Proxy
 import java.util.concurrent.TimeUnit
 
+class CloseInterceptor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+        var request = chain.request().newBuilder().addHeader("Connection", "close").build()
+        return chain.proceed(request)
+    }
+}
 
 interface StatusService {
     @GET("api/status")
     suspend fun getStatus() : Response<Status>
 
     companion object {
-        /*private val proxy = Proxy(Proxy.Type.HTTP, InetSocketAddress("192.168.1.1", 80))
         private val okHttpClient: OkHttpClient? = OkHttpClient.Builder()
-            .proxy(proxy)
-            .connectTimeout(20, TimeUnit.SECONDS)
-            .build()*/
+            .addNetworkInterceptor(CloseInterceptor())
+            .build();
         private const val BASE_URL = "http://192.168.1.1/"
         fun create() : StatusService {
             return Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
+                .client(okHttpClient)
                 .build()
                 .create(StatusService::class.java)
         }
