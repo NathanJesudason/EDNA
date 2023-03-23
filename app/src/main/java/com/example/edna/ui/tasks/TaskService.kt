66052f6274b7255eb1c66b4fd28924e336510116
Bryan <nguyenb6@oregonstate.edu>
2023-03-23 00:04:15 -0700

    Forgot to change one thing to status

--- app/src/main/java/com/example/edna/ui/tasks/TaskService.kt
+++ app/src/main/java/com/example/edna/ui/tasks/TaskService.kt
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit
 
 interface TaskService {
 
-    @GET("api/taasks")
+    @GET("api/tasks")
     suspend fun getStatus() : Response<Tasks>
 
     companion object {
